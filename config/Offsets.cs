58c2e971dcd04e875583cf35b269fb049d1d8bcb
P-pisiko <144721929+P-pisiko@users.noreply.github.com>
2025-07-01 14:59:36 -0400

    Updated Offsets

--- config/Offsets.cs
+++ config/Offsets.cs
@@ -10,12 +10,18 @@ namespace CoD4_dm1.config
     {
         public static IntPtr FpsCounterAddress { get; } =  0xC6EE228;
 
-        public static IntPtr Cam_X { get; } = 0x00797648;
+        public static IntPtr Cam_X { get; } = 0x39B6A4;
 
-        public static IntPtr Cam_Y { get; } = 0x0079764C;
+        public static IntPtr Cam_Y { get; } = 0x85B6F4;
 
-        public static IntPtr Cam_Z_Player { get; } = 0x00797650;
+        public static IntPtr Cam_Z { get; } = 0x3973F8;
 
-        public static IntPtr Cam_Z { get; } = 0x85B6F4;
+        public static IntPtr Cam_Y_Player { get; } = 0x85B6F4;
+
+        public static IntPtr Cam_Yaw { get; } = 0x39B69C;
+
+        public static IntPtr Cam_Pitch { get; } = 0x39B698;
+
+
     }
 }
