e3a8d4ee6046d8668de2b5fb33c7066776e896c0
P-pisiko <144721929+P-pisiko@users.noreply.github.com>
2025-07-03 16:30:22 -0400

    Swaped offsets

--- config/Offsets.cs
+++ config/Offsets.cs
@@ -12,9 +12,9 @@ namespace CoD4_dm1.config
 
         public static IntPtr Cam_X { get; } = 0x39B6A4;
 
-        public static IntPtr Cam_Y { get; } = 0x85B6F4;
+        public static IntPtr Cam_Y { get; } = 0x3973F8;
 
-        public static IntPtr Cam_Z { get; } = 0x3973F8;
+        public static IntPtr Cam_Z { get; } = 0x85B6F4;
 
         public static IntPtr Cam_Y_Player { get; } = 0x85B6F4;
 
