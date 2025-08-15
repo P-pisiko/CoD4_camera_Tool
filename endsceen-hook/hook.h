ed2ff8e04ada50d69258d5d60ee03c9156463806
P-pisiko <144721929+P-pisiko@users.noreply.github.com>
2025-08-14 21:03:13 -0400

    header files

---
+++ endsceen-hook/hook.h
@@ -0,0 +1,12 @@
+#pragma once
+#ifndef HOOK_H
+#define HOOK_H
+
+#include <Windows.h>
+#include <iostream>
+#include <string>
+
+void* WINAPI hookFn(char* hookedFn, char* hookFn, int copyBytesSize, unsigned char* backupBytes, std::string descr);
+BOOL WINAPI restore(char* fn, unsigned char* writeBytes, int writeSize, std::string descr);
+
+#endif
