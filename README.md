948e5dbbf2ac129a56e99e0be5aba582ffc5acf9
P-pisiko <enesfidan75@proton.me>
2025-08-22 16:27:02 -0400

    client init

--- README.md
+++
@@ -1,3 +0,0 @@
-# CoD4-dm1
-
-## This is still work in progress. First release will be soon.
--- endsceen-hook/injectedThread.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-#ifndef HAX_H
-#define HAX_H
-#include <windows.h>
-#include "d3d.h"
-
-void WINAPI injectedThread(HMODULE hModule);
-
-#endif
