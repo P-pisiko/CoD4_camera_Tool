42386d8063e115474b378a29500c8f061ba263f7
P-pisiko <enesfidan75@proton.me>
2025-08-22 22:07:12 -0400

    client program for testing server functionalities.

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,4 @@
-// main.cpp : This file contains the 'main' function. Program execution begins and ends there.
+// main.cpp : This file contains the 'main' function. 
 //
 
 #include <iostream>
@@ -10,13 +10,50 @@ HANDLE connectToPipe(const char*);
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    std::cout << "Hello World!\n"; // for good luck
+
     const char* pipeName = R"(\\.\pipe\MyPipe)";
 
+    HANDLE hPipe = connectToPipe(pipeName);
 
+    OVERLAPPED overlapped = {};
+    //overlapped.hEvent = CreateEvent(NULL, TRUE, FALSE, NULL);
+    overlapped.hEvent = NULL;
+
+
+    while (!GetAsyncKeyState(VK_ESCAPE)) {
+        SHORT keyState = GetAsyncKeyState(VK_NUMPAD5);
+        std::int16_t v = (keyState & 0x8000) ? 1 : 0; // high bit set if pressed |  0x8000: Hold down some time | 0x0001 press sinse last checked
+
+        OVERLAPPED ov = {};
+        DWORD bytesWritten = 0;
+
+        BOOL success = WriteFile(
+            hPipe,
+            &v,
+            sizeof(v),
+            &bytesWritten,
+            &ov
+        );
+
+        if (!success) {
+            DWORD err = GetLastError();
+            if (err == ERROR_IO_PENDING) {
+                std::cout << "Write pending, dont really care..\n";
+            }
+            else {
+                std::cerr << "WriteFile failed. Error: " << err << std::endl;
+                break;
+            }
+        }
+        else {
+            std::cout << "Sent: " << v << " (" << bytesWritten << " bytes)\n";
+        }
+        Sleep(150);
+    }
 }
 
-void console() {
+void console() { // 
     AllocConsole();
     FILE* f;
     freopen_s(&f, "CONOUT$", "w", stdout);
