d4a90af8f7f5bb63d3dda6463990cadf3444b925
P-pisiko <enesfidan75@proton.me>
2025-08-22 21:58:31 -0400

    input read

--- src/main.cpp
+++ src/main.cpp
@@ -2,19 +2,33 @@
 //
 
 #include <iostream>
+#include <Windows.h>
 
 int main()
 {
     std::cout << "Hello World!\n";
 }
 
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
+void console() {
+    AllocConsole();
+    FILE* f;
+    freopen_s(&f, "CONOUT$", "w", stdout);
+    freopen_s(&f, "CONIN$", "r", stdin);
+    std::cout << "Console Created" << std::endl;
+    while (true) {
+        std::string input;
+        std::cin >> input;
 
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
+        if (input == "exit") {
+            break;
+        }
+        if (input == "") {
+            continue;
+
+        }
+        if (input == "") {
+            continue;
+        }
+    }
+    FreeConsole();
+}
