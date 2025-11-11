9a3c75848b3b9170d14fca4c412793023da82be8
P-pisiko <enesfidan75@proton.me>
2025-11-10 20:52:17 -0500

    cam properties fixed + a lot of refactoring

--- FileFormats/GlTF.cs
+++ FileFormats/GlTF.cs
@@ -1,12 +1,9 @@
-﻿
-using SharpGLTF.Animations;
-using SharpGLTF.Scenes;
+﻿using SharpGLTF.Scenes;
 using SharpGLTF.Schema2;
 using System.Numerics;
 
 namespace CoD4_dm1.FileFormats
 {
-
     public class GlTF
     {
         private const float INC_TO_METRE = 0.0254f;
@@ -16,24 +13,18 @@ namespace CoD4_dm1.FileFormats
         public static Task ExportToGLB(Structs.Entitys.Header header, List<Structs.Entitys.Camera> camList)
         {
             var flipY = false;
-
             var scene = new SceneBuilder();
+
+            #region Camera Properties
             const float VerticalFOV = 80f * DEG_TO_RAD;
             const float ZNear = 0.5f;
             const float ZFar = 1000f;
-            var camBuilder = new CameraBuilder.Perspective(VerticalFOV, ZNear, ZFar);
-
-            var first = camList[0];
-            var camPos0 = new Vector3(
-                first.X * INC_TO_METRE,
-                (flipY ? -first.Y : first.Y) * INC_TO_METRE,
-                first.Z * INC_TO_METRE
-            );
+            var camBuilder = new CameraBuilder.Perspective(null, VerticalFOV, ZNear, ZFar ); //16f / 9f
+            #endregion
 
             scene.AddCamera(camBuilder, Matrix4x4.Identity);
             var model = scene.ToGltf2();
             var camNode = model.LogicalNodes.FirstOrDefault(n => n.Camera != null);
-            
             
             var translationKeys = new List<(float time, Vector3 value)>(camList.Count);
             var rotationKeys = new List<(float time, Quaternion value)>(camList.Count);
@@ -48,7 +39,7 @@ namespace CoD4_dm1.FileFormats
                 float z = (flipY ? f.Y : -f.Y) * INC_TO_METRE;  // Game Y becomes Blender -Z
                 var pos = new Vector3(x, y, z);
 
-                var quat = ConvertCod4ToBlenderQuaternion(f.Yaw, f.Pitch);
+                var quat = Cod4ToQuaternion(f.Yaw, f.Pitch);
 
                 translationKeys.Add((time, pos));
                 rotationKeys.Add((time, quat));
@@ -64,25 +55,26 @@ namespace CoD4_dm1.FileFormats
                 camNode.WithRotationAnimation("Camera_Animation", rotationKeys.ToArray());
                 camNode.WithTranslationAnimation("Camera_Animation", translationKeys.ToArray());
             }
-
+            #region Shit everything happened here to disk.
             var MapName = string.IsNullOrWhiteSpace(header.MapName) ? "map" : header.MapName;
-            var filename = $"{MapName}_fixed.glb";
+
+            var filename = $"{MapName}_{header.ConstCaptureFps}fps_{DateTime.Now.ToString("HH-mm-ss")}.glb";
 
             if (!Directory.Exists("./exported_cams"))
             {
                 Directory.CreateDirectory("./exported_cams");
             }
-
             model.SaveGLB($"./exported_cams/{filename}");
-            Console.WriteLine($"Done Writing to glb file: {filename}. Frames:{camList.Count} FPS:{header.ConstCaptureFps}");
-
+            Console.WriteLine($"Done Writing glb file: {filename}. Frames:{camList.Count} FPS:{header.ConstCaptureFps}");
+            #endregion
             return Task.CompletedTask;
+            
         }
-
-        private static Quaternion ConvertCod4ToBlenderQuaternion(float yawDeg, float pitchDeg)
-        {
+        #region Euler to Quaternion Conversion stuff
+        private static Quaternion Cod4ToQuaternion(float yawDeg, float pitchDeg)
+        {   // Turning CoD4's wierd pitch deg to signed so its always positive
             float signedPitchDeg = (pitchDeg <= 85f) ? -pitchDeg : 360f - pitchDeg;
-
+            
             float pitchRad = signedPitchDeg * DEG_TO_RAD;
             float yawRad = yawDeg * DEG_TO_RAD;
 
@@ -90,15 +82,21 @@ namespace CoD4_dm1.FileFormats
             var quatPitch = Quaternion.CreateFromAxisAngle(Vector3.UnitX, pitchRad);
             var quatYaw = Quaternion.CreateFromAxisAngle(Vector3.UnitY, yawRad);
 
-            // Combine: pitch first, then yaw (world-yaw)
+            // Combine: pitch first, then yaw (i have no idea what this does..)
             var q = Quaternion.Normalize(Quaternion.Multiply(quatYaw, quatPitch));
-            // +90° yaw offset
+            // -90° yaw offset
             float yawOffsetRad = -90f * DEG_TO_RAD;
             var quatOffset = Quaternion.CreateFromAxisAngle(Vector3.UnitY, yawOffsetRad);
 
             return Quaternion.Normalize(Quaternion.Multiply(quatOffset, q));
         }
-
+        /// <summary>
+        /// NOT USED ANYMORE
+        /// </summary>
+        /// <param name="x"></param>
+        /// <param name="y"></param>
+        /// <param name="z"></param>
+        /// <returns></returns>
         private static Quaternion EulerXYZToQuaternion(float x, float y, float z)
         {
             // XYZ Euler to Quaternion conversion
@@ -118,5 +116,7 @@ namespace CoD4_dm1.FileFormats
 
             return quat;
         }
+
+        #endregion
     }
 }
